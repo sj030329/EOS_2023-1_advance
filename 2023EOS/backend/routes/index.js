8e7313634dd52816be94516bc3647714230e6abd
Seokjin Lee <110400683+sj030329@users.noreply.github.com>
2023-05-22 19:32:21 +0900

    Update index.js

--- 2023EOS/backend/routes/index.js
+++ 2023EOS/backend/routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+
+const User = require('./user');
+
+router.use('/user', User);
+
 router.get('/', function(req, res, next) {
 res.send('hello world!!');
 });
